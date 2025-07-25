d62d038305935aee2328ecef71f49b807e305f28
jaypatel70star <monparajayashokbhai2004@gmail.com>
2025-07-26 00:04:29 +0530

    Delete cart.js

--- cart.js
+++
@@ -1,7 +0,0 @@
-let cart = JSON.parse(localStorage.getItem("cakecart")) || [];
-function addToCart(name, price) {
-    const item = {name, price};
-    cart.push(item);
-    localStorage.setItem("cakecart", JSON.stringify(cart));
-    alert(`${name} added to cart!`);
-}
