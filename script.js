f2da92da857b40f11722b29b2f3c3243ab3603a1
jaypatel70star <monparajayashokbhai2004@gmail.com>
2025-07-26 14:23:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,26 +6,20 @@ const accountMenu = document.getElementById("account-menu");
 const isloggedin = localStorage.getItem("user");
 accountMenu.innerHTML = isloggedin ? `<a href="#" onclick="logout()">Logout</a>` : `<a href="login.html">Login</a><a href="register.html">Register</a>`;
 
-function logout() {
-    localStorage.removeItem("user");
-    alert("You have been logged out.");
-    window.location.href = "login.html";
-}
-
 function toggledropdown(e) {
     e.preventDefault();
     accountMenu.classList.toggle("show-dropdown");
 }
 
-const menutoggle = document.getElementById("menu-toggle");
-const navbar = getElementById("navbar");
+function logout() {
+    localStorage.removeItem("user");
+    alert("you have been logged out.");
+    window.location.href = "login.html";
+}
 
-menutoggle.addEventListener("click", () => {
-    navbar.classList.toggle("active");
-});
-
-document.addEventListener("click", function (e) {
-    if(!this.document.getElementById("account-dropdown").contains(e.target)) {
+document.addEventListener("click", function(e) {
+    const dropdown = document.getElementById("account-dropdown");
+    if(!dropdown.contains(e.target)) {
         accountMenu.classList.remove("show-dropdown");
     }
 });
